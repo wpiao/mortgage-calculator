bb00066d1f5a2d8a2a23ca4723ab3541ae73249b
Piao <wenhaopi@08f8bc6186e1.ant.amazon.com>
2021-04-01 07:17:02 -0700

    Comment out test file temporarilu

--- test/com/javaccinations/calculator/MortgageCalculatorTest.java
+++ test/com/javaccinations/calculator/MortgageCalculatorTest.java
@@ -1,23 +1,23 @@
-package com.javaccinations.calculator;
-
-import org.junit.Before;
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
-public class MortgageCalculatorTest {
-
-    @Before
-    public void setUp() {
-        Calculator calc = CalcFactory.createCalculator("Purchase");
-    }
-
-    @Test
-    public void calculate() {
-        Mortgage mort1 = new Mortgage()
-    }
-
-    @Test
-    public void display() {
-    }
-}
+// package com.javaccinations.calculator;
+//
+// import org.junit.Before;
+// import org.junit.Test;
+//
+// import static org.junit.Assert.*;
+//
+// public class MortgageCalculatorTest {
+//
+//     @Before
+//     public void setUp() {
+//         Calculator calc = CalcFactory.createCalculator("Purchase");
+//     }
+//
+//     @Test
+//     public void calculate() {
+//         Mortgage mort1 = new Mortgage()
+//     }
+//
+//     @Test
+//     public void display() {
+//     }
+// }
